11365d265af20c70779da6de89dbd7758e02a04b
Andrew Darqui <andrew.darqui@gmail.com>
2015-07-04 23:46:17 -0400

    spofksdfpokds

--- haskell/data/data/src/maybe.hs
+++
@@ -1,9 +0,0 @@
-import Data.Typeable
-import Data.Data
-
-main :: IO ()
-main = do
- print $ dataTypeOf $ Just (1 :: Integer)
- print $ dataTypeConstrs $ dataTypeOf $ Just "Hi"
- print $ isAlgType $ dataTypeOf $ Just (1 :: Integer)
- print $ isAlgType $ dataTypeOf (1 :: Integer)
